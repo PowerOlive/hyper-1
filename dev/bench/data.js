75c0ace5c8e7c7ad444e74521fcd43df7b61916f
github-action-benchmark <github@users.noreply.github.com>
2021-01-23 13:48:47 +0000

    add pipeline (cargo) benchmark result for 1928682b33f98244435ba6d574677546205a15ec

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1611409726852,
+  "repoUrl": "https://github.com/PowerOlive/hyper-1",
+  "entries": {
+    "pipeline": [
+      {
+        "commit": {
+          "author": {
+            "email": "sean@seanmonstar.com",
+            "name": "Sean McArthur",
+            "username": "seanmonstar"
+          },
+          "committer": {
+            "email": "sean@seanmonstar.com",
+            "name": "Sean McArthur",
+            "username": "seanmonstar"
+          },
+          "distinct": true,
+          "id": "1928682b33f98244435ba6d574677546205a15ec",
+          "message": "feat(ffi): add HYPERE_INVALID_PEER_MESSAGE error code for parse errors",
+          "timestamp": "2021-01-20T15:10:19-08:00",
+          "tree_id": "e87d140b4320ce0f482aa7ee06ddeb97a4822f19",
+          "url": "https://github.com/PowerOlive/hyper-1/commit/1928682b33f98244435ba6d574677546205a15ec"
+        },
+        "date": 1611409723222,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "hello_world_16",
+            "value": 44899,
+            "range": "± 3749",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
