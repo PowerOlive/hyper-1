350278ada4624fb4fb436e5b4f5916ea60c55268
github-action-benchmark <github@users.noreply.github.com>
2021-02-19 02:03:22 +0000

    add connect (cargo) benchmark result for f01de8e503c649034a53a8fdf3ad2e107ad9ad2d

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1612939064333,
+  "lastUpdate": 1613700201844,
   "repoUrl": "https://github.com/PowerOlive/hyper-1",
   "entries": {
     "pipeline": [
@@ -1389,6 +1389,36 @@ window.BENCHMARK_DATA = {
             "name": "http_connector",
             "value": 42551,
             "range": "± 2233",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "markus.westerlind@distilnetworks.com",
+            "name": "Markus Westerlind",
+            "username": "Marwes"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f01de8e503c649034a53a8fdf3ad2e107ad9ad2d",
+          "message": "refactor(client): Use async/await more (#2437)\n\n* refactor: Use async/await in client.rs\r\n\r\n* refactor: Simplify client.rs a bit more\r\n\r\n* refactor: Allow !Unpin in Lazy\r\n\r\n* Remove some impl Future\r\n\r\n* Remove some combinator use",
+          "timestamp": "2021-02-18T10:35:43-08:00",
+          "tree_id": "ecce068928942422a611eea6f2e2c7533022f73d",
+          "url": "https://github.com/PowerOlive/hyper-1/commit/f01de8e503c649034a53a8fdf3ad2e107ad9ad2d"
+        },
+        "date": 1613700197692,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "http_connector",
+            "value": 51382,
+            "range": "± 1554",
             "unit": "ns/iter"
           }
         ]
