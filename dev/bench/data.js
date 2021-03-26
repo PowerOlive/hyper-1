14ae5693fcbfbb24995494180ba00ecc55a3e26d
github-action-benchmark <github@users.noreply.github.com>
2021-03-26 06:54:23 +0000

    add connect (cargo) benchmark result for 51ed71b0a6e1d90d324f87151434334bab6fb837

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1616498647275,
+  "lastUpdate": 1616741662811,
   "repoUrl": "https://github.com/PowerOlive/hyper-1",
   "entries": {
     "pipeline": [
@@ -3369,6 +3369,36 @@ window.BENCHMARK_DATA = {
             "name": "http_connector",
             "value": 60215,
             "range": "± 7655",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "vagelis.prokopiou@gmail.com",
+            "name": "Vagelis Prokopiou",
+            "username": "Vagelis-Prokopiou"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "51ed71b0a6e1d90d324f87151434334bab6fb837",
+          "message": "docs(client): use Method::POST to match the example in https://hyper.rs/guides/client/advanced/ (#2479)",
+          "timestamp": "2021-03-25T13:34:03-07:00",
+          "tree_id": "908a1fb4a47378d16439458035a25a2dd41c1516",
+          "url": "https://github.com/PowerOlive/hyper-1/commit/51ed71b0a6e1d90d324f87151434334bab6fb837"
+        },
+        "date": 1616741658485,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "http_connector",
+            "value": 43112,
+            "range": "± 4349",
             "unit": "ns/iter"
           }
         ]
