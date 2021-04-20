94bb666d39f7b8dac335b268bb56e587e196bb49
github-action-benchmark <github@users.noreply.github.com>
2021-04-20 22:43:26 +0000

    add connect (cargo) benchmark result for 11345394d968d4817e1a0ee2550228ac0ae7ce74

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1617856962658,
+  "lastUpdate": 1618958605988,
   "repoUrl": "https://github.com/PowerOlive/hyper-1",
   "entries": {
     "pipeline": [
@@ -4359,6 +4359,36 @@ window.BENCHMARK_DATA = {
             "name": "http_connector",
             "value": 43186,
             "range": "± 3291",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "123095+nox@users.noreply.github.com",
+            "name": "Anthony Ramine",
+            "username": "nox"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "11345394d968d4817e1a0ee2550228ac0ae7ce74",
+          "message": "feat(client): add option to allow misplaced spaces in HTTP/1 responses (#2506)",
+          "timestamp": "2021-04-20T14:17:48-07:00",
+          "tree_id": "3b29f8a6165231bdbe3449acf1c70aeb16bc9153",
+          "url": "https://github.com/PowerOlive/hyper-1/commit/11345394d968d4817e1a0ee2550228ac0ae7ce74"
+        },
+        "date": 1618958601422,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "http_connector",
+            "value": 51361,
+            "range": "± 1014",
             "unit": "ns/iter"
           }
         ]
