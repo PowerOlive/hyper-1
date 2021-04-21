4f933d74c5c07af92ac033c547accbf103aca4c9
github-action-benchmark <github@users.noreply.github.com>
2021-04-21 10:47:02 +0000

    add connect (cargo) benchmark result for 117cc492a62c4051c75e7eec0f624b30db8a20e5

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1619001976489,
+  "lastUpdate": 1619002021835,
   "repoUrl": "https://github.com/PowerOlive/hyper-1",
   "entries": {
     "pipeline": [
@@ -4695,6 +4695,36 @@ window.BENCHMARK_DATA = {
             "name": "http_connector",
             "value": 51361,
             "range": "± 1014",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sean@seanmonstar.com",
+            "name": "Sean McArthur",
+            "username": "seanmonstar"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "117cc492a62c4051c75e7eec0f624b30db8a20e5",
+          "message": "chore(ci): quiet the warnings when verifying the generated header file (#2507)",
+          "timestamp": "2021-04-20T17:23:10-07:00",
+          "tree_id": "77b730b8de4c7597554f528b8cebaf09aabd0796",
+          "url": "https://github.com/PowerOlive/hyper-1/commit/117cc492a62c4051c75e7eec0f624b30db8a20e5"
+        },
+        "date": 1619002018355,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "http_connector",
+            "value": 54525,
+            "range": "± 14332",
             "unit": "ns/iter"
           }
         ]
